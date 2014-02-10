ecf3a9cfe97ef213073ae2cab2b903ae5ef676c6
Jonathan <Jonathan@192.168.0.105>
2014-02-10 16:48:19 -0200

    mudei nada so para testar

--- intelligentCode/src/exception/ObjectNotFoundException.java
+++ intelligentCode/src/exception/ObjectNotFoundException.java
@@ -4,6 +4,7 @@ public class ObjectNotFoundException extends Exception {
 	private final static long serialVersionUID = 1;
 	
 	public ObjectNotFoundException() {
+		
 	}
 
 	public ObjectNotFoundException(String msg) {
